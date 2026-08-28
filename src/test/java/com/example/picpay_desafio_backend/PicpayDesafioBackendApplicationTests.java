7c640f145471ada8e480c47f25747caca1123ea2
yasbenevenuto <yasmin.b.holando@gmail.com>
2026-08-28 11:22:44 -0300

    commit inicial - estrutura do projeto Spring Boot

---
+++ src/test/java/com/example/picpay_desafio_backend/PicpayDesafioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.picpay_desafio_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayDesafioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
